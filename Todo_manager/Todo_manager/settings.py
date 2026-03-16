17cc848b65092e2ab31878ce7c55c41d853daee5
Rishabh <pradhanrishabh101@gmail.com>
2026-03-17 03:21:03 +0530

    switch database to sqlite

--- Todo_manager/Todo_manager/settings.py
+++ Todo_manager/Todo_manager/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'django-insecure-hb@h(fv2!3^b5gs-1u=7r&rm!yrq7k!z-bb=t94$=4(dft^9^c
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
     "iamrishabh.linkpc.net",
     "www.iamrishabh.linkpc.net"
 ]
